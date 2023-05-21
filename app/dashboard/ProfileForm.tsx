75794ae3b3b8104a105fa27b0b8cd164e86bda98
ctcallahan2@crimson.ua.edu <ctcallahan2@crimson.ua.edu>
2023-05-20 20:22:47 -0500

    can save photos to an album

--- app/dashboard/ProfileForm.tsx
+++ app/dashboard/ProfileForm.tsx
@@ -1,32 +1,45 @@
 "use client";
 
+import { prisma } from "@/lib/prisma";
 import { User } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 interface Props {
   user: User;
 }
 
 export default function ProfileForm({ user }: Props) {
-  const updateUser = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
+  //Implements a server action here, this will be called on the server when the form on the client is submitted
+  const updateUser = async (formData: FormData) => {
+    "use server";
     const body = {
       name: formData.get("name") as string,
       email: formData.get("email") as string,
     };
 
-    const res = await fetch("/api/user", {
-      method: "PUT",
-      body: JSON.stringify(body),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    await prisma.user.update({ where: { id: user.id }, data: body });
+
+    //Revalidates the dashboard page, so that the new data is fetched from the server
+    //If you add a loading ui here, then it will be automatically ran when the form is submitted
+    //and while the data is being revalidated
+    revalidatePath("/dashboard");
+  };
+
+  //Implements a server action here, this will be called on the server when the form on the client is submitted
+  const updateUserAndRoute = async (formData: FormData) => {
+    "use server";
+    const body = {
+      name: formData.get("name") as string,
+      email: formData.get("email") as string,
+    };
+    await prisma.user.update({ where: { id: user.id }, data: body });
+    redirect("/");
   };
 
   return (
     <div>
-      <form onSubmit={updateUser}>
+      <form action={updateUser}>
         <label htmlFor="name">name</label>
         <input
           type="text"
@@ -42,6 +55,7 @@ export default function ProfileForm({ user }: Props) {
           defaultValue={user?.email ?? ""}
         />
         <button type="submit">Save</button>
+        <button formAction={updateUserAndRoute}>Save and go to home</button>
       </form>
     </div>
   );
